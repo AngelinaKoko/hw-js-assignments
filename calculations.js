f248d7d286fd4ac307cf895748a882818268e983
AK <barsukn@gmail.com>
2023-01-27 15:10:10 +0100

    added decrement, increment

--- calculations.js
+++ calculations.js
@@ -18,7 +18,17 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function getIncrementBalance(x) {
+  x = x++;
+  return x;
+}
+
+function getDecrementBalance(x) {
+  return --x;
+}
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(getIncrementBalance(3000));
+console.log(getDecrementBalance(3000));
